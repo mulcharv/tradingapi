c2fdbe4ff57adffdf22b44da1f55064a869458ec
Rahul Mulchandani <44975642+mulcharv@users.noreply.github.com>
2023-08-31 16:07:43 +0100

    update

--- app.js
+++ app.js
@@ -284,9 +284,7 @@ app.put('/portfolio/:stockid', upload.any(), passport.authenticate('jwt',  {sess
     }
   }
   }
-  if (action === 'buy') {
-      res.json('hello');
-  }
+  res.json(exists)
 }));
 
 app.get('/account/:userid', passport.authenticate('jwt',  {session: false}), asyncHandler(async(req, res, next) => {
