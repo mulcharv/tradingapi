189c44bec7a7ed779a6ba96e685c1b8d88c28d9d
Rahul Mulchandani <44975642+mulcharv@users.noreply.github.com>
2023-09-06 00:35:39 +0100

    update user model

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     username: { type: String, required: true, unique: true, maxLength: 50},
     password: { type: String, required: true, maxLength: 100},
+    watchlist: { type: Array }
 });
 
 
