e8710c7572fb6a49caf9cb6cfdf501722f052b8f
Rahul Mulchandani <44975642+mulcharv@users.noreply.github.com>
2023-09-05 22:59:30 +0100

    update account and portfolio models

--- models/account.js
+++ models/account.js
@@ -2,9 +2,20 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const { DateTime } = require("luxon");
+
+
 const AccountSchema = new Schema({
     balance: { type: Number, required: true, min: 0},
     user: { type: Schema.Types.ObjectId, ref: "User" }
+}, {timestamps: true, toObject: {virtuals: true}, toJSON: {virtuals: true} });
+
+AccountSchema.virtual("createdAt_formatted").get(function () {
+    return DateTime.fromJSDate(this.createdAt).toLocaleString(DateTime.DATE_MED);
+});
+
+AccountSchema.virtual("updatedAt_formatted").get(function () {
+    return DateTime.fromJSDate(this.updatedAt).toLocaleString(DateTime.DATE_MED);
 });
 
 
--- models/portfolio.js
+++ models/portfolio.js
@@ -4,6 +4,9 @@ const Position = require('./position').schema;
 
 const Schema = mongoose.Schema;
 
+const { DateTime } = require("luxon");
+
+
 const PortfolioSchema = new Schema({
     positions: [
             Position
@@ -12,6 +15,14 @@ const PortfolioSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: "User" }
 
 
+}, {timestamps: true, toObject: {virtuals: true}, toJSON: {virtuals: true} });
+
+PortfolioSchema.virtual("createdAt_formatted").get(function () {
+    return DateTime.fromJSDate(this.createdAt).toLocaleString(DateTime.DATE_MED);
+});
+
+PortfolioSchema.virtual("updatedAt_formatted").get(function () {
+    return DateTime.fromJSDate(this.updatedAt).toLocaleString(DateTime.DATE_MED);
 });
 
 
